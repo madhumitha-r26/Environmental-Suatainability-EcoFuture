129cf1aab821af5aac2d039bd36c2a57139cd916
madhumitha-ravichandran26 <madhumitha26301@gmail.com>
2024-11-15 18:57:24 +0530

    final commit

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ navbarToggler.addEventListener('click', () => {
   navbarHamburger.classList.toggle('d-none');
   navbarClose.classList.toggle('d-block');
 });
+
+function donate() {
+  var amount = document.getElementById("exampleFormControlInput1").value;
+  alert("Thanks for donating Rs." + amount);
+  var modal = bootstrap.Modal.getInstance(document.getElementById('exampleModal'));
+  modal.hide();
+}
